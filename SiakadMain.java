5a550be4207f191ec4d57bfb4f18c934576db30e
rafiadpr <work.rafiadrianp@gmail.com>
2025-04-17 16:30:57 +0700

    add main file

---
+++ SiakadMain.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SiakadMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int pilihan;
+
+        do { 
+            System.out.println("=== MENU SISTEM AKADEMIK ===");
+        } while (pilihan != 0);
+    }
+}
