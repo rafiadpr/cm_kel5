046d00386cecb115d6e84c4a941b3cca4793da23
rafiadpr <work.rafiadrianp@gmail.com>
2025-04-17 16:39:08 +0700

    add switch case for choice

--- SiakadMain.java
+++ SiakadMain.java
@@ -1,12 +1,41 @@
+
 import java.util.Scanner;
 
 public class SiakadMain {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pilihan;
 
-        do { 
+        do {
             System.out.println("=== MENU SISTEM AKADEMIK ===");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata Kuliah");
+            System.out.println("3. Tampilkan Data Penilaian");
+            System.out.println("4. Urutkan Mahasiwa Berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = input.nextInt();
+            input.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    System.out.println("Program Selesai! Terima kasih!");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid!");
+            }
         } while (pilihan != 0);
     }
 }
