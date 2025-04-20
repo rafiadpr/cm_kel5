4fd91af87ac53a1d76802d2862bb5276a2c84ce4
Yahya1805 <belajarkuliah902@gmail.com>
2025-04-20 17:44:41 +0700

    perubahan penambahan array

--- Matakuliah05.java
+++ Matakuliah05.java
@@ -9,7 +9,20 @@ public class Matakuliah05 {
         this.sks = sks;
     }
 
-    public void tampilMataKuliah() {
-        System.out.println("Kode MK: " + kodeMK + " | Nama: " + namaMK + " | SKS: " + sks);
+    public static Matakuliah05[] daftarMK = new Matakuliah05[10]; // Bisa tampung 10 matkul
+    public static int jumlahMK = 0;
+
+    static {
+        // Isi manual
+        daftarMK[jumlahMK++] = new Matakuliah05("MK001", "Aljabar", 2);
+        daftarMK[jumlahMK++] = new Matakuliah05("MK002", "Struktur Data", 3);
+        daftarMK[jumlahMK++] = new Matakuliah05("MK003", "Basis Data", 3);
+        daftarMK[jumlahMK++] = new Matakuliah05("MK004", "Desain Web", 3);
+    }
+
+    public static void tampilDaftarMataKuliah() {
+        for (int i = 0; i < jumlahMK; i++) {
+            System.out.println("Kode: " + daftarMK[i].kodeMK + ", Nama: " + daftarMK[i].namaMK + ", SKS: " + daftarMK[i].sks);
+        }
     }
 }
