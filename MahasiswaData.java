cea287e9ac886fd9332ded7a9842c1d83df5a333
rafiadpr <work.rafiadrianp@gmail.com>
2025-04-20 21:11:24 +0700

    changes on mahasiswa data and main

--- MahasiswaData.java
+++ MahasiswaData.java
@@ -7,6 +7,16 @@ public class MahasiswaData {
         mahasiswa[0] = new Mahasiswa("22001", "Ali Rahman", "Informatika");
         mahasiswa[1] = new Mahasiswa("22002", "Budi Santoso", "Informatika");
         mahasiswa[2] = new Mahasiswa("22003", "Citra Dewi", "Sistem Informasi Bisnis");
+
+        matakuliah[0] = new Matakuliah05("MK001", "Struktur Data", 3);
+        matakuliah[1] = new Matakuliah05("MK002", "Basis Data", 3);
+        matakuliah[2] = new Matakuliah05("MK003", "Desain Web", 3);
+
+        penilaian[0] = new Penilaian(mahasiswa[0], matakuliah[0], 80, 85, 90);
+        penilaian[1] = new Penilaian(mahasiswa[0], matakuliah[1], 60, 75, 70);
+        penilaian[2] = new Penilaian(mahasiswa[1], matakuliah[0], 75, 70, 80);
+        penilaian[3] = new Penilaian(mahasiswa[2], matakuliah[1], 85, 90, 95);
+        penilaian[4] = new Penilaian(mahasiswa[2], matakuliah[2], 80, 90, 65);
     }
 
     public void tampilkanMahasiswa() {
@@ -15,8 +25,10 @@ public class MahasiswaData {
         }
     }
 
-    public void setPenilaian(Penilaian[] penilaian) {
-        this.penilaian = penilaian;
+    public void tampilkanMatakuliah() {
+        for (Matakuliah05 mk : matakuliah) {
+            mk.tampil();
+        }
     }
 
     public void tampilkanPenilaian() {
